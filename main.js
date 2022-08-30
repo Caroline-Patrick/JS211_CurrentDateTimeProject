af6b811db3d45430a6f2e5e43944dcaaa52b3c5d
Caroline-Patrick <carolinespatrick@gmail.com>
2022-08-29 20:59:13 -0500

    changed functions to correct format for node

--- main.js
+++ main.js
@@ -15,47 +15,56 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
-let usersNumber = ""
-let numInputString =  document.getElementById('numberInputHere')
+// let usersNumber = ""
+// let numInputString =  document.getElementById('numberInputHere')
 
-const saveValue = (numInput) => {
-  usersNumber = numInput.value
+// const saveValue = (numInput) => {
+//   usersNumber = numInput.value
+//   console.log(typeof(usersNumber))
+//   console.log(usersNumber.toString());
 
-  console.log(usersNumber.toString());
+// }
 
+// const numberToStringSubmit =()=>{
+//   console.log('submitted')
+//   let numNowString = document.createElement('LI');
+//   numNowString.innerText = usersNumber
+//   numInputString.appendChild(numNowString)
+// }
+
+const numToString = (n) => {
+  console.log(`line 36 stringToNumber:  ${typeof(n)}`)
+  const num = n.toString()
+  console.log( `line 38 stringToNumber:  ${typeof(num)}`)
 }
 
-const numberToStringSubmit =()=>{
-  console.log('submitted')
-
-  let numNowString = document.createElement('LI');
-
-  numNowString.innerText = usersNumber
-
-  numInputString.appendChild(numNowString)
-}
+numToString(2);
 
 // Write a JavaScript program to convert a string to the number.
-let numberString = ""
-let stringInput =  document.getElementById('stringInputHere')
+// let numberString = ""
+// let stringInput =  document.getElementById('stringInputHere')
 
-const saveValueForString = (stringInp) => {
-  usersNumberString = stringInp.value
+// const saveValueForString = (stringInp) => {
+//   usersNumberString = stringInp.value
+//   console.log(usersNumberString.toString());
 
-  console.log(usersNumberString.toString());
+// }
 
+// const stringToNumber =()=>{
+//   console.log('submitted')
+//   let stringNowNum = document.createElement('LI');
+//   stringNowNum.innerText = usersNumberString
+//   stringInput.appendChild(stringNowNum)
+// }
+const stringToNumber =(s)=>{
+  console.log(`line 60 stringToNumber:  ${typeof(s)}`);
+  const num = parseInt(s);
+  console.log(num)
+  console.log(`line 63 stringToNumber: ${typeof(num)}`);
+ 
 }
 
-const stringToNumber =()=>{
-  console.log('submitted')
-
-  let stringNowNum = document.createElement('LI');
-
-  stringNowNum.innerText = usersNumberString
-
-  stringInput.appendChild(stringNowNum)
-}
-
+stringToNumber('emily')
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -64,72 +73,95 @@ const stringToNumber =()=>{
   // * Number
   // * NaN
   // * String
-const oneVar = true
-const twoVar = null
-const threeVar= undefined
-const fourVar=5
-const fiveVar= NaN
-const sixVar= "hi there"
 
-const dataTypeFinder = (inputVar) => {
-  console.log('typeof variable: ', typeof inputVar )
+//Boolean
+const dataTypes=(v) => {
+  switch (typeof(v)) {
+    case 'boolean':
+      console.log(`${v} is a boolean!`)
+      break;
+    case null:
+      console.log(`${v} is null!`)
+      break;
+    case undefined:
+      console.log(`${v} is undefined`)
+      break;
+    case 'number':
+      console.log(`${v} is a number`)
+      break;
+    case NaN:
+      console.log(`${v} is not a number`)
+      break;    
+    case 'string':
+      console.log(`${v} is a string!`) 
+      break;
+    default:
+      console.log(`I don't know what ${v} is.`)
+      break;
+  }
 }
+ dataTypes([1,2,3])
 
-let dataInput = ""
-let dataOutputElement =  document.getElementById('dataInputHere')
+// const dataTypeFinder = (inputVar) => {
+//   console.log('typeof variable: ', typeof inputVar )
+// }
 
-const saveData = (dataInp) => {
-  usersDataInput = dataInp.value
+// let dataInput = ""
+// let dataOutputElement =  document.getElementById('dataInputHere')
+// const saveData = (dataInp) => {
+//   usersDataInput = dataInp.value
 
-  console.log('Variable Type: ', typeof usersDataInput);
+//   console.log('Variable Type: ', typeof usersDataInput);
 
-}
+// }
 
-const dataIdentify =()=>{
-  console.log('submitted')
+// const dataIdentify =()=>{
+//   console.log('submitted')
+//   let dataItem = document.createElement('LI');
 
-  let dataItem = document.createElement('LI');
-
-  dataItem.innerText = usersDataInput + ` is a ${typeof usersDataInput}` ; 
-  dataOutputElement.appendChild(dataItem);
-}
+//   dataItem.innerText = usersDataInput + ` is a ${typeof usersDataInput}` ; 
+//   dataOutputElement.appendChild(dataItem);
+// }
 
 // Write a JavaScript program that adds 2 numbers together.
 
-let firstNum = null
-let secondNum = null
-let operation = null
-const saveFirstNumber = (num) => {
-  firstNum = parseInt(num)  
-}
-const saveSecondNumber = (num) => {
-  // "parseInt" is a built in function in JS that converts a string/word into a number
-  secondNum = parseInt(num)
-}
+// let firstNum = null
+// let secondNum = null
+// let operation = null
+// const saveFirstNumber = (num) => {
+//   firstNum = parseInt(num)  
 
-const add = (numA, numB) => {
+// }
+// const saveSecondNumber = (num) => {
+//   // "parseInt" is a built in function in JS that converts a string/word into a number
+//   secondNum = parseInt(num)
+// }
+
+const addTwo = (numA, numB) => {
   const sum = numA + numB
-  return sum
+  console.log(sum)
 }
 
-const changeOperation = (chosenOperation) => {
-  operation = chosenOperation
-  console.log(operation)
-}
+addTwo(4,5)
 
-const putResultInElement = (operationResults) => {
+// const changeOperation = (chosenOperation) => {
+//   operation = chosenOperation
+//   console.log(operation)
+// }
 
-  document.getElementById("result").innerHTML = "Results: " + operationResults
-}
-const equals = () => {
-  switch (operation) {
-    case "addition":  putResultInElement(add(firstNum, secondNum)) 
-    break;
-    case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
-    break;
-    default: "Choose an operation"
-  }
-}
+// const putResultInElement = (operationResults) => {
+
+//   document.getElementById("result").innerHTML = "Results: " + operationResults
+// }
+// const equals = () => {
+//   switch (operation) {
+//     case "addition":  putResultInElement(add(firstNum, secondNum)) 
+//     break;
+//     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
+//     break;
+//     default: "Choose an operation"
+//   }
+// }
 
 // Write a JavaScript program that runs only when 2 things are true.
 
@@ -138,11 +170,8 @@ const truthSeeker =(arg1, arg2)=> {
 
 if (arg1 && arg2 == true) {
   console.log("Both are TRUE")
-}
-else {
-  console.log("At least one is FALSE")
-}
-}
+}}
+
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
